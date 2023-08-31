38e4dc6522bfc2003ebdca871c7b9b261ee7acac
00jup <jupark0369@gmail.com>
2023-08-31 15:53:46 +0900

    dialog SizedBox, onPressed:(){ Navigator.pop(context) }

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-      MaterialApp(
-          home: MyApp()));
+  runApp(MaterialApp(home: MyApp()));
 }
 
 class MyApp extends StatefulWidget {
@@ -31,31 +29,56 @@ class _MyAppState extends State<MyApp> {
           onPressed: () {
             print(context.findAncestorWidgetOfExactType<MaterialApp>());
             print('1');
-            showDialog(context: context, builder: (context) {
-              return Dialog(child: Text("hello"));
-            });
+            showDialog(
+                context: context,
+                builder: (context) {
+                  return Dialog(
+                      child: SizedBox(
+                          width: 300,
+                          height: 300,
+                          child: Column(
+                            children: [
+                              Text(
+                                "Contact",
+                                style: TextStyle(
+                                    fontSize: 20, fontWeight: FontWeight.w400),
+                              ),
+                              TextField(),
+                              Container(
+                                child: Row(
+                                  children: [
+                                    TextButton(
+                                        onPressed: () {
+                                          Navigator.pop(context);
+                                        },
+                                        child: Text("Cancel")),
+                                    TextButton(
+                                        onPressed: () {}, child: Text("OK")),
+                                  ],
+                                ),
+                              )
+                            ],
+                          )));
+                });
           },
         ),
         appBar: AppBar(),
         body: Container(
             child: ListView.builder(
-              itemCount: 3,
-              itemBuilder: (context, i) {
-                //print(i); //debuggin 가능함.
-                return ListTile(
-                  leading: Icon(Icons.contact_page),
-
-                  title: Text(
-                    "박정욱",
-                    style: TextStyle(
-                        fontSize: 40.0, fontWeight: FontWeight.w300),
-                  ),
-
-                );
-              },
-            )
-          //i for문에서 개수 증가하는 것처럼 i가 증가한다. 목록 많이 필요할 때 ListView.builder
-        ));
+          itemCount: 3,
+          itemBuilder: (context, i) {
+            //print(i); //debuggin 가능함.
+            return ListTile(
+              leading: Icon(Icons.contact_page),
+              title: Text(
+                "박정욱",
+                style: TextStyle(fontSize: 40.0, fontWeight: FontWeight.w300),
+              ),
+            );
+          },
+        )
+            //i for문에서 개수 증가하는 것처럼 i가 증가한다. 목록 많이 필요할 때 ListView.builder
+            ));
   }
 }
 
